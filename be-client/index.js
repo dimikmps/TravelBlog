148420da390b3bff0b7fbaf097591327f84fa853
dimi_kmps <dimitris.kmps@gmail.com>
2021-06-25 20:27:13 +0300

    Add error handling logic in Parse init phase

--- be-client/index.js
+++ be-client/index.js
@@ -14,53 +14,51 @@ var ParseDashboard = require('parse-dashboard');
 var app = express();
 app.use(cors());
 
-// TODO: Add local db fallback
+// TODO: Add fallback
 // const databaseUri = process.env.DB_URI || process.env.MONGODB_URI;
 const databaseUri = process.env.DB_URI;
 
-// TODO: Add error handling logic
-if (!databaseUri) {
-  console.log('DATABASE_URI not specified!');
+try {
+  var api = new ParseServer({
+    databaseURI: databaseUri,
+    appId: process.env.APP_ID,
+    masterKey: process.env.MASTER_KEY,
+    fileKey: process.env.FILE_KEY,
+    serverURL: process.env.SERVER_URL
+  });
+
+  var dashboard = new ParseDashboard({
+    "apps": [
+      {
+        "serverURL": process.env.SERVER_URL,
+        "appId": process.env.APP_ID,
+        "masterKey": process.env.MASTER_KEY,
+        "appName": process.env.APP_NAME,
+      }
+    ],
+    "users": [
+      {
+        "user": process.env.APP_USER,
+        "pass": process.env.APP_PASS
+      }
+    ]
+  });
+
+  // Serve the Parse API on the /parse URL prefix, using the pre-defined api config
+  app.use('/parse', api);
+
+  // make the Parse Dashboard available at /dashboard, , using the pre-defined dashboard config
+  app.use('/dashboard', dashboard);
+
+  const httpServer = require('http').createServer(app);
+
+  httpServer.listen(port, function () {
+    console.log('Initializing parse-server on port ' + port + '!');
+  });
+
+} catch (error) {
+  console.log('\n>>> Parse Server/Dashboard init failed with:', error, '\n');
 }
-
-// TODO: Add error handling logic
-var api = new ParseServer({
-  databaseURI: databaseUri,
-  appId: process.env.APP_ID,
-  masterKey: process.env.MASTER_KEY, 
-  fileKey: process.env.FILE_KEY,
-  serverURL: process.env.SERVER_URL
-});
-
-// TODO: Add error handling logic
-var dashboard = new ParseDashboard({
-  "apps": [
-    {
-      "serverURL": process.env.SERVER_URL,
-      "appId": process.env.APP_ID,
-      "masterKey": process.env.MASTER_KEY,
-      "appName": process.env.APP_NAME,
-    }
-  ],
-  "users": [
-    {
-      "user": process.env.APP_USER,
-      "pass": process.env.APP_PASS
-    }
-  ]
-});
-
-// Serve the Parse API on the /parse URL prefix, using the pre-defined api config
-app.use('/parse', api);
-
-// make the Parse Dashboard available at /dashboard, , using the pre-defined dashboard config
-app.use('/dashboard', dashboard);
-
-const httpServer = require('http').createServer(app);
-
-httpServer.listen(port, function () {
-  console.log('Initializing parse-server on port ' + port + '!');
-});
 
 module.exports = {
   app
