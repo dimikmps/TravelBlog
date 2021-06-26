4cd89fe1310a4bf66695ce4f4acd6e5a58112baf
dimi_kmps <dimitris.kmps@gmail.com>
2021-06-26 03:00:50 +0300

    Add create-landmark component, add navBar

--- fe-client/src/app/app.module.ts
+++ fe-client/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { LandmarkCreateComponent } from './landmarks/landmark-create/landmark-create.component';
+import { HeaderComponent } from './header/header.component'
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandmarkCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatToolbarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
